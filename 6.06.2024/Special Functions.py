d3be94877a6af8b167c680efe37fc863b7327ead
Tanya290 <161779215+Tanya290@users.noreply.github.com>
2024-06-08 14:38:47 +0530

    Add files via upload

---
+++ 6.06.2024/Special Functions.py
@@ -0,0 +1,15 @@
+# Special Functions
+keys = ["key1", "key2", "key3"]
+
+# len()
+print(len(keys))  # Output: 3
+
+# id()
+print(id(keys))  # Output: Unique ID of the list object
+
+# type()
+print(type(keys))  # Output: <class 'list'>
+
+# range()
+for i in range(8):
+    print(i)  # Output: 0 1 2 3 4 5 6 7
